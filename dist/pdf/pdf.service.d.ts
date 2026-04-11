406a28fd5a5016f5e330560bd5655af970a96867
Zenx007 <zenxpokas@gmail.com>
2026-04-11 14:42:56 -0300

    chore: remove tracked node_modules and dist

--- dist/pdf/pdf.service.d.ts
+++
@@ -1,8 +0,0 @@
-export declare class PdfService {
-    convertPdfBufferToHtml(pdfBuffer: Buffer): Promise<string>;
-    convertHtmlToPdfBuffer(html: string): Promise<Buffer>;
-    isValidHtml(html: string): boolean;
-    private textToHtml;
-    private isHeadingCandidate;
-    private escapeHtml;
-}
